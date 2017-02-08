5acd9ae2958d1a752bca222357878922c62097a1
Vladislav Samsonov <vvladxx@gmail.com>
2017-02-08 09:56:09 +0300

    .

--- packman/rss.py
+++ packman/rss.py
@@ -6,11 +6,11 @@ import pickle
 
 def select_majority(a):
 	c = {}
-	for x in a:
+	for x, weight in a:
 		if x in c:
-			c[x] += 1
+			c[x] += weight
 		else:
-			c[x] = 1
+			c[x] = weight
 	mc = 0
 	mx = c.keys()[0]
 	for x in c:
@@ -26,20 +26,21 @@ def load(f):
 
 
 class RSSTrigger:
-	def __init__(self, w, bias, action):
+	def __init__(self, w, bias, action, beta):
 		self.w = w
 		self.bias = bias
 		self.action = action
+		self.beta = beta
 
 	def is_triggered(self, obs):
 		return self.w.ravel().dot(obs.ravel()) < self.bias
 
 	def get_action(self):
-		return self.action
+		return (self.action, self.beta)
 
 
 class RSS:
-	def __init__(self, step_fn, first_obs_fn, obs_dims, action_space=[0], cmr_repeats=9, alpha=0.9, alpha_decay=0.95):
+	def __init__(self, step_fn, first_obs_fn, obs_dims, action_space=[0], cmr_repeats=9, alpha=0.9, alpha_decay=0.95, beta=1.0, beta_multiplier=1.05):
 		self.step_fn = step_fn
 		self.first_obs_fn = first_obs_fn
 		self.obs_dims = obs_dims
@@ -48,6 +49,8 @@ class RSS:
 		self.cmr_repeats = cmr_repeats
 		self.alpha = alpha
 		self.alpha_decay = alpha_decay
+		self.beta = beta
+		self.beta_multiplier = beta_multiplier
 
 	def get_random_action(self):
 		return self.action_space[random.randrange(0, len(self.action_space))]
@@ -88,11 +91,11 @@ class RSS:
 		biases.sort()
 		return biases[int(len(biases) * self.alpha)]
 
-	def train(self, backup_intervals=60*2, trig_search_repeats=100, model_file_name='model.pkl'):
+	def train(self, backup_intervals=60*2, trig_search_repeats=100, model_file_name='model.pkl', w_init=np.random.normal):
 		last_backup_time = time.time()
 		while True:
 			cmr = self.get_current_mean_reward(self.cmr_repeats)
-			print('Current mean reward: %f; learned triggers: %d; alpha: %.3f' % (cmr, len(self.triggers), self.alpha))
+			print('Current mean reward: %f; learned triggers: %d; alpha: %.3f; beta: %.3f' % (cmr, len(self.triggers), self.alpha, self.beta))
 			if time.time() > last_backup_time + backup_intervals:
 				last_backup_time = time.time()
 				print('Saving model file...')
@@ -102,8 +105,8 @@ class RSS:
 			best_trig = None
 			assert(len(self.action_space) <= trig_search_repeats)
 			for i in range(trig_search_repeats):
-				w = np.random.normal(size=self.obs_dims)
-				trig = RSSTrigger(w, self.get_optimal_bias(w), self.get_action(i))
+				w = w_init(size=self.obs_dims)
+				trig = RSSTrigger(w, self.get_optimal_bias(w), self.get_action(i), self.beta)
 				self.triggers.append(trig)
 				reward = self.get_current_mean_reward(1)
 				if best_reward < reward:
@@ -116,3 +119,4 @@ class RSS:
 			if best_trig is not None:
 				self.triggers.append(best_trig)
 			self.alpha *= self.alpha_decay
+			self.beta *= self.beta_multiplier
--- packman/train.py
+++ packman/train.py
@@ -1,5 +1,6 @@
 import rss
 import gym
+import argparse
 
 env = gym.make('MsPacman-v0')
 
@@ -10,5 +11,13 @@ def step_fn(action):
 	observation, reward, done, _ = env.step(action)
 	return observation, reward, done
 
-r = rss.RSS(step_fn, first_obs_fn, (210,160,3), range(9), 20, alpha=0.2, alpha_decay=0.997)
-r.train()
+parser = argparse.ArgumentParser()
+parser.add_argument('--alpha', type=float, default=0.5)
+parser.add_argument('--ad', type=float, default=0.96)
+parser.add_argument('--bm', type=float, default=1.05)
+parser.add_argument('--save', type=str, default='model.pkl')
+
+args = parser.parse_args()
+
+r = rss.RSS(step_fn, first_obs_fn, (210,160,3), range(9), 20, alpha=args.alpha, alpha_decay=args.ad, beta_multiplier=args.bm)
+r.train(2 * 60, 500, args.save)
